4cdceaca0b8e6e980e9a10642c23903256fa4558
lavanyac102 <52200833+lavanyac102@users.noreply.github.com>
2019-07-30 16:33:50 +0530

    Update DemoClass.java

--- src/test/DemoClass.java
+++ src/test/DemoClass.java
@@ -4,10 +4,7 @@ public class DemoClass {
 
 	public static void mian(String [] args)
 	{
-		System.out.println("Lavanya is selenium class");
-		
-		System.out.println("Lavanya will go out from selenium class");
-		System.out.println("Lavanya will go out from selenisdjfgdskfskjum class");
+		System.out.println("hiiiiiiiiiiiiiii");
 	}
 	
 }
