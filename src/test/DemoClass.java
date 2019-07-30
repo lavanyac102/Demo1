fa93fc41ca93cae75b86dd56ada450117a002ff7
lavanyac102 <52200833+lavanyac102@users.noreply.github.com>
2019-07-30 16:42:24 +0530

    Update DemoClass.java

--- src/test/DemoClass.java
+++ src/test/DemoClass.java
@@ -4,7 +4,7 @@ public class DemoClass {
 
 	public static void mian(String [] args)
 	{
-		System.out.println("in selenium class");
+		System.out.println("in selenium jgjgclass");
 		System.out.println("in selenium classhgdjhgfghfjtf");
 
 	}
