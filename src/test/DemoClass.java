b4974d71a230a9e976ef7d6285503102790710c7
lavanyac102 <52200833+lavanyac102@users.noreply.github.com>
2019-07-30 15:45:18 +0530

    Update DemoClass.java

--- src/test/DemoClass.java
+++ src/test/DemoClass.java
@@ -4,10 +4,10 @@ public class DemoClass {
 
 	public static void mian(String [] args)
 	{
-		System.out.println("Lavanya is selenium class");
+		System.out.println("in selenium class");
 		
-		System.out.println("Lavanya will go out from selenium class");
-		System.out.println("Lavanya is selenium class, wil go to cv raman after class");
+		System.out.println("not in classgg");
+
 	}
 	
 }
